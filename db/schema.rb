c5491f8baa29d56682effaafc995b8c05ee9b079
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-01-22 17:46:30 -0500

    Step: 2 AddCityAndStateToLocations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122221421) do
+ActiveRecord::Schema.define(version: 20150122223439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20150122221421) do
     t.string "name"
     t.string "created_at"
     t.string "updated_at"
+    t.text   "city"
+    t.text   "state"
   end
 
 end
