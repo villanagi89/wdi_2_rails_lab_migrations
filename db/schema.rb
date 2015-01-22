bee6eea28cf7ba764bd10a16dd7e5fc7034e3546
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-01-22 17:50:41 -0500

    Step4 - AddColorToLocations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122223439) do
+ActiveRecord::Schema.define(version: 20150122224708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150122223439) do
     t.string "updated_at"
     t.text   "city"
     t.text   "state"
+    t.text   "color"
   end
 
 end
